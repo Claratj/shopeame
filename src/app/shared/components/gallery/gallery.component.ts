c46476de31dfae1689c83a5ec0637a622496ecd4
i-am-Claratj <clara@claratoloba.com>
2021-01-17 12:50:37 +0100

    puliendo vista en lista

--- src/app/shared/components/gallery/gallery.component.ts
+++ src/app/shared/components/gallery/gallery.component.ts
@@ -9,13 +9,39 @@ export class GalleryComponent implements OnInit {
 
   changeView: string;
 
+  figureChange : string;
+
+  ratingBuy :string;
+
+  info: string;
+
+  listActive: string;
+
   @Input() list:any;
 
   constructor() { }
 
   ngOnInit(): void {
 
+    // this.gridView();
+    this.listView();
+    
+  }
   
+  gridView(){
+    this.changeView = 'col-12 col-sm-6 col-md-4 col-lg-3 card';
+    this.figureChange = 'p-gallery__figure-grid';
+    this.ratingBuy = 'justify-content-around '
+
+  }
+
+  listView() {
+    this.changeView = 'col-12 d-flex';
+    this.listActive = '';
+    this.figureChange = 'p-gallery__figure-list align-items-center ';
+    this.info = ''
+    this.ratingBuy = 'justify-content-between'
+
   }
 
 }
